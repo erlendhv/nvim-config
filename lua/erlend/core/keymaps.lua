23b40994c563fb72bf90b0fa1ece61fc9a041e23
Erlend Veire <erlend.veire@gmail.com>
2024-11-22 18:23:57 +0000

    tmux navigation with nvim

--- lua/erlend/core/keymaps.lua
+++ lua/erlend/core/keymaps.lua
@@ -1,43 +1,45 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
-
 local keymap = vim.keymap -- for conciseness
 
-
 -- Define the modes you want to map to
-local modes = { 'n', 'v', 'o' }  -- Normal, Visual, and Operator-pending modes
+local modes = { "n", "v", "o" } -- Normal, Visual, and Operator-pending modes
 
 -- Function to map keys in multiple modes
 local function map_in_modes(modes, lhs, rhs, opts)
-  for _, mode in ipairs(modes) do
-    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
-  end
+	for _, mode in ipairs(modes) do
+		vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+	end
 end
 --
 -- -- Options for non-recursive and silent mapping
 local opts = { noremap = true, silent = true }
 
 -- Remap movement keys
-map_in_modes(modes, 'j', 'h', opts)
-map_in_modes(modes, 'k', 'j', opts)
-map_in_modes(modes, 'l', 'k', opts)
-map_in_modes(modes, 'ø', 'l', opts)
+map_in_modes(modes, "j", "h", opts)
+map_in_modes(modes, "k", "j", opts)
+map_in_modes(modes, "l", "k", opts)
+map_in_modes(modes, "ø", "l", opts)
 
 -- Remap the original functions of j, k, l
-map_in_modes(modes, 'h', 'j', opts)
-map_in_modes(modes, 'J', 'k', opts)
-map_in_modes(modes, 'K', 'l', opts)
+map_in_modes(modes, "h", "j", opts)
+map_in_modes(modes, "J", "k", opts)
+map_in_modes(modes, "K", "l", opts)
 
 -- Disable the default keybindings first
 vim.g.tmux_navigator_no_mappings = 1
 
 -- Custom keybindings using your preferred keys (ctrl + jklp)
-vim.api.nvim_set_keymap('n', '<C-j>', ':TmuxNavigateLeft<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', ':TmuxNavigateDown<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':TmuxNavigateUp<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<C-p>', ':TmuxNavigateRight<CR>', { silent = true, noremap = true })
-
+-- vim.api.nvim_set_keymap("n", "<M-j>", ":TmuxNavigateLeft<CR>", { silent = true, noremap = true })
+-- vim.api.nvim_set_keymap("n", "<M-k>", ":TmuxNavigateDown<CR>", { silent = true, noremap = true })
+-- vim.api.nvim_set_keymap("n", "<M-l>", ":TmuxNavigateUp<CR>", { silent = true, noremap = true })
+-- vim.api.nvim_set_keymap("n", "<M-ø>", ":TmuxNavigateRight<CR>", { silent = true, noremap = true })
+-- vim.api.nvim_set_keymap("n", "<M-p>", ":TmuxNavigateRight<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<A-j>", ":<C-U>TmuxNavigateLeft<cr>", opts)
+vim.keymap.set("n", "<A-k>", ":<C-U>TmuxNavigateDown<cr>", opts)
+vim.keymap.set("n", "<A-l>", ":<C-U>TmuxNavigateUp<cr>", opts)
+vim.keymap.set("n", "<A-ø>", ":<C-U>TmuxNavigateRight<cr>", opts)
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
