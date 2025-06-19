767752f3477149d59b19a5f190a5d2d513ab5de3
Erlend Hafslund Veire <erlendveire@Erlend-sin-MacBook-Pro.local>
2025-06-19 15:01:42 +0200

    changed linter for ts and js

--- lua/erlend/plugins/linting.lua
+++ lua/erlend/plugins/linting.lua
@@ -1,29 +1,40 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "mason.nvim" }, -- Keep the dependency for good measure
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
-    }
+		lint.linters_by_ft = {
+			javascript = { "biome" },
+			typescript = { "biome" },
+			javascriptreact = { "biome" },
+			typescriptreact = { "biome" },
+			python = { "pylint" },
+		}
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		-- The keymap for manual triggering is fine to set up immediately
+		vim.keymap.set("n", "<leader>ll", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+		-- ** THE FIX IS HERE **
+		-- We will create the autocommands that trigger automatically
+		-- only AFTER Neovim has fully loaded.
+		vim.api.nvim_create_autocmd("VimEnter", {
+			pattern = "*",
+			once = true, -- Only run this once per Neovim session
+			callback = function()
+				-- We create a new autocommand group here inside the callback
+				local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+				vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+					group = lint_augroup,
+					callback = function()
+						-- Use require inside the callback just to be safe
+						require("lint").try_lint()
+					end,
+				})
+			end,
+		})
+	end,
 }
